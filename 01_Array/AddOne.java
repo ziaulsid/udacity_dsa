e18f881d28b838ff7fbe9f8329c6ad4d3f9fa519
ziaulsiddiqui <ziauls42@gmail.com>
2020-06-25 21:19:35 +0530

    Add-one solution

---
+++ 01_Array/AddOne.java
@@ -0,0 +1,20 @@
+class MaximumSumSubarray{
+    public int[] plusOne(int[] digits) {
+        
+        int length = digits.length;
+        int[] result = new int[length + 1];
+      
+        
+        for(int i = length-1; i >= 0; i--){
+            
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        
+        result[0]++;
+        return result;
+    } 
+}
