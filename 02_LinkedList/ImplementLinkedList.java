9a58f8629f16eafb56b23f1f60e95267d8637f33
ziaulsiddiqui <ziauls42@gmail.com>
2020-06-30 00:33:27 +0530

    Limited solution

--- 02_LinkedList/ImplementLinkedList.java
+++ 02_LinkedList/ImplementLinkedList.java
@@ -9,6 +9,7 @@ class MyLinkedList {
         
         public Node(int val){
             this.val = val;
+            this.next = null;
         }
     }
 
@@ -22,14 +23,23 @@ class MyLinkedList {
     public int get(int index) {
         
         if(head == null) return -1;
+        if(index == 0) return head.val;
         else{
-            
+            int count = 0;
             Node temp = head;
             while(temp.next != null){
                 temp = temp.next;
+                count++;
+                
+                if(count == index){
+                    break;
+                }
+                else {
+                    return -1;
+                }
             }
+            return temp.val;
         }
-        return -1;
     }
     
     /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
@@ -50,25 +60,63 @@ class MyLinkedList {
     /** Append a node of value val to the last element of the linked list. */
     public void addAtTail(int val) {
         
+        Node node = new Node(val);
+        if(head == null){
+            head = node;
+        }else{
+            
+           Node temp = head;
+           while(temp.next != null){
+                temp = temp.next;
+           }
+           
+            temp.next = node;
+            
+            
+        }
     }
     
     /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
     public void addAtIndex(int index, int val) {
+        
+        if(head == null) return;
+        Node newNode = new Node(val);
+        int count = 0;
+        Node temp = head;
+        while(temp.next != null){
+            count++;
+            if(count == index) {
+                
+                newNode.next = temp.next;
+                temp.next = newNode;
+                
+            } else{
+                
+                temp = temp.next;
+            }
+            
+        } 
         
     }
     
     /** Delete the index-th node in the linked list, if the index is valid. */
     public void deleteAtIndex(int index) {
         
+        if(head == null) return;
+        
+        int count = 0;
+        Node temp = head;
+        Node prev = head;
+        while(temp.next != null){
+            temp = temp.next;
+            count++;
+            if(count == index) {
+                
+                prev.next = temp.next;
+                temp.next = null;
+                
+            } 
+            
+        } 
     }
 }
-
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * MyLinkedList obj = new MyLinkedList();
- * int param_1 = obj.get(index);
- * obj.addAtHead(val);
- * obj.addAtTail(val);
- * obj.addAtIndex(index,val);
- * obj.deleteAtIndex(index);
- */
